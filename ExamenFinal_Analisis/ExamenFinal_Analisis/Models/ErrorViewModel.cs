51257e7224d6c16c5fb0b4f8dda72019c7c2d4b7
CrischCode <cchamomorales@gmail.com>
2026-06-11 22:46:00 -0600

    Subiendo proyecto

---
+++ ExamenFinal_Analisis/ExamenFinal_Analisis/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenFinal_Analisis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
